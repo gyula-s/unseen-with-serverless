3e8fb71f2eee4a719f80625a6acbcda4553813ae
Gyula S <10959738+gyula-s@users.noreply.github.com>
2023-09-17 17:05:58 +0100

    chore: add todo tests

--- src/controllers/superhero.test.js
+++ src/controllers/superhero.test.js
@@ -1,5 +1,7 @@
 describe('superheroController', () => {
   it.todo('should respont with 400 if no score or invalid score is provided');
 
-  it.todo('returns a csv');
+  it.todo('returns a json');
+
+  it.todo('saves csv to s3');
 });
