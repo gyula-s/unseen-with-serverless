a206b8e543984749a2cf0ae8f87f29d57d1928f4
Gyula S <10959738+gyula-s@users.noreply.github.com>
2023-09-17 12:57:32 +0100

    clarify tasks before attempting writing code

--- src/controllers/superhero.ts
+++ src/controllers/superhero.ts
@@ -8,5 +8,25 @@ export const scoreController = async (req, res, _next) => {
     res.status(400).json({ error: 'INVALID INPUT' });
     return;
   }
+
+  /**
+   * 1. using fetch get a user from: https://randomuser.me/api/
+   * 2. calculate the invisibility score:
+   * Male genderWeighting: 5, Female genderWeighting: 8
+   * Invisibility Score = genderWeighting * (superHeroTestScore - age)
+   * normalise score between 0 and 100
+   * 3. calculate invisibility status based on the score:
+   * 0 - 20 : Not invisible
+   * 20 - 40 : Camouflage
+   * 40 - 60 : Translucent
+   * 60 - 80 : Transparent
+   * 80 - 100 : Invisible
+   * 4. generate CVS file with the following columns:
+   * user data(gender, name, dob, age,), superhero test score, invisibility score and status.
+   * return the csv file as a response
+   * 5. consider API GW, Cognito, or custom code in the handler to authenticate the request
+   * 6. consider large traffic and how to handle it
+   * 7. consider logging, monitoring, and alerting
+   */
   res.json({ msg: 'SUCCESS' });
 };
